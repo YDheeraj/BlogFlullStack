8bdbe0084597a95eb30012a039e095fbf19de4b6
Dheeraj Kumar Yadav <yadavdheeraj2002@gmail.com>
2024-10-18 00:14:15 +0530

    updated folder structure for deployment

---
+++ app/api/latestposts/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import prisma from "/utils/connect";
+
+export const GET = async (req) => {
+    try {
+        // Fetch the latest 3 posts
+        const latestPosts = await prisma.post.findMany({
+            take: 3,
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
+
+        return new NextResponse(JSON.stringify({ latestPosts }), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 500 });
+    }
+};
